7bf528c25e3d91954cb41a44f7b5ef3ba0190741
Shoko Kimera <shokokimera@gmail.com>
2023-07-26 07:26:02 +0300

    place pdf content in table form

--- src/PDFGenerator.jsx
+++ src/PDFGenerator.jsx
@@ -1,31 +1,94 @@
 import React from 'react';
-import { PDFViewer, Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+import { PDFViewer, Page, Document, StyleSheet, PDFDownloadLink, View, Text } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4',
+    flexDirection: 'column',
+    backgroundColor: '#FFFFFF',
     padding: 20,
   },
   section: {
     flexGrow: 1,
-  },
-  text: {
     marginBottom: 10,
+  },
+  table: {
+    display: 'table',
+    width: '100%',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  tableRow: {
+    flexDirection: 'row',
+  },
+  tableCellTextHeader: {
+    width: '66.67%',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  tableCellPredictionHeader: {
+    width: '33.33%',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  textCell: {
+    width: '66.67%',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    textAlign: 'left',
+    fontSize: 10,
+  },  
+  predictionCell: {
+    width: '33.33%',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    textAlign: 'center',
+    fontSize: 10,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  titleText: {
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
 
 const PDFContent = ({ data }) => (
   <Document>
-    {console.log(data)}
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        {data.map((item) => (
-          <View key={item.id} style={{ marginBottom: 10 }}>
-            <Text style={styles.text}>Text: {item.text}</Text>
-            <Text style={styles.text}>Prediction: {item.prediction}</Text>
+        <Text style={{ fontSize: 24, textAlign: 'center', marginBottom: 10 }}>Report</Text>
+        <Text style={{ fontSize: 16, textAlign: 'center' }}>Observations and Results</Text>
+      </View>
+
+      <View style={styles.section}>
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCellTextHeader}>Text</Text>
+            <Text style={styles.tableCellPredictionHeader}>Prediction</Text>
           </View>
-        ))}
+          {data.map((item) => (
+            <View style={styles.tableRow} key={item.id}>
+              <Text style={styles.textCell}>{item.text}</Text>
+              <Text style={styles.predictionCell}>{item.prediction}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </Page>
   </Document>
